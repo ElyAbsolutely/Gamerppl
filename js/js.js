b21814199db9272a1e0af73094a48a51f1ec8f55
JojoSnow <milzwow94@gmail.com>
2021-08-31 11:02:34 +0300

    little bit

--- js/js.js
+++ js/js.js
@@ -1,3 +1,18 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext;
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
 
+function drawWalls() {    
+    ctx.fillRect(20, 20, 40, 40);
+    ctx.fillStyle = 'orange';
+    ctx.translate
+}
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    drawWalls();
+
+    requestAnimationFrame(update);
+}
+
+update();
