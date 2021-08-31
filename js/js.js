cb976816af8689751c5008179a0d509fd1083698
Janne <jannemieskolainen@gmail.com>
2021-08-31 11:36:32 +0300

    movement keys

--- js/js.js
+++ js/js.js
@@ -21,6 +21,37 @@ const player = {
     png: null //Halutaanko kuvat myöhemmin?
 }
 
+document.addEventListener("keydown", function (event) {
+    //move
+    if (event.key === "w") {
+        //go up
+        console.log("W")
+    } else if (event.key === "a") {
+        //go left
+        console.log("A")
+    } else if (event.key === "s") {
+        //go down
+        console.log("S")
+    } else if (event.key === "d") {
+        //go right
+        console.log("D")
+    }
+    //attack
+    if (event.key === "ArrowUp") {
+        //attact up
+        console.log("keyUp")
+    } else if (event.key === "ArrowLeft") {
+        //attack left
+        console.log("keyLeft")
+    } else if (event.key === "ArrowDown") {
+        //attack down
+        console.log("keyDown")
+    } else if (event.key === "ArrowRight") {
+        //attack right
+        console.log("keyRight")
+    }
+})
+
 function drawPlayer() {
     ctx.fillStyle = player.color;
     ctx.fillRect(player.x, player.y, player.w, player.h);
