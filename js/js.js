de7707c8fd88590337405766b97ce6abe82ae008
Janne <jannemieskolainen@gmail.com>
2021-08-31 10:00:15 +0300

    js and hmtl changes

---
+++ js/js.js
@@ -0,0 +1,8 @@
+var game = document.getElementById("game");
+var ctx = game.getContext("2d");
+
+document.getElementById("button").addEventListener("click", start);
+
+function start() {
+    console.log("Hello");
+}
