4a0540fae8c61e18065a4f28669aac6b09ce8146
Janne <jannemieskolainen@gmail.com>
2021-08-31 14:17:36 +0300

    attack stages

--- js/js.js
+++ js/js.js
@@ -11,6 +11,8 @@ function start() {
 var midx = canvas.width / 2;
 var midy = canvas.height / 2;
 
+var weapon01 = document.getElementById("dagger");
+
 const player = {
     x: midx - 10,
     y: midy - 10,
@@ -18,6 +20,7 @@ const player = {
     w: 20,
     speed: 1,
     color: "red",
+    attackFrames: -1, //max 25
     png: null //Halutaanko kuvat myöhemmin?
 }
 
@@ -31,7 +34,7 @@ const wall = [
         color: 'blue',
         dx: 0,
         dy: 0
-    }, 
+    },
     {
         x: 100,
         y: 0,
@@ -49,9 +52,64 @@ function drawPlayer() {
     ctx.fillRect(player.x, player.y, player.w, player.h);
 }
 
-function drawWalls() { 
+var attackUp = false;
+var attackLeft = false;
+var attackRight = false;
+var attackDown = false;
+
+function attack(dir) {
+    if (player.attackFrames == -1) {
+        switch (dir) {
+            case 1:
+                attackUp = true;
+                console.log("keyUp");
+                break;
+            case 2:
+                attackLeft = true;
+                console.log("keyLeft");
+                break;
+            case 3:
+                attackDown = true;
+                console.log("keyDown");
+                break;
+            case 4:
+                attackRight = true;
+                console.log("keyRight");
+                break;
+        }
+        player.attackFrames = 0;
+    }
+}
+
+function drawAttack() {
+    console.log(player.attackFrames);
+    if (player.attackFrames > -1) {
+        if (attackUp) {
+            ctx.drawImage(weapon01, player.x, player.y - player.h, player.h, player.w);
+        } else if (attackLeft) {
+            ctx.drawImage(weapon01, player.x - player.w, player.y, player.h, player.w);
+        } else if (attackDown) {
+            ctx.drawImage(weapon01, player.x, player.y + player.h, player.h, player.w);
+        } else if (attackRight) {
+            ctx.drawImage(weapon01, player.x + player.w, player.y, player.h, player.w);
+        }
+
+        player.attackFrames++;
+
+        if (player.attackFrames == 25) {
+            player.attackFrames = -1;
+
+            attackUp = false;
+            attackLeft = false;
+            attackRight = false;
+            attackDown = false;
+        }
+    }
+}
+
+function drawWalls() {
     for (let i = 0; i < wall.length; i++) {
-        ctx.fillStyle = wall[i].color;   
+        ctx.fillStyle = wall[i].color;
         ctx.fillRect(wall[i].x, wall[i].y, wall[i].w, wall[i].h);
     }
 }
@@ -101,16 +159,16 @@ document.addEventListener("keydown", function (event) {
     //attack
     if (event.key === "ArrowUp") {
         //attact up
-        console.log("keyUp")
+        attack(1);
     } else if (event.key === "ArrowLeft") {
         //attack left
-        console.log("keyLeft")
+        attack(2);
     } else if (event.key === "ArrowDown") {
         //attack down
-        console.log("keyDown")
+        attack(3);
     } else if (event.key === "ArrowRight") {
         //attack right
-        console.log("keyRight")
+        attack(4);
     }
 })
 
@@ -119,8 +177,9 @@ function update() {
 
     drawPlayer();
     drawWalls();
-    
+    drawAttack();
+
     newPos();
-    
+
     requestAnimationFrame(update);
 }
