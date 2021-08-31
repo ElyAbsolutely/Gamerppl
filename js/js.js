d1f82f4e394a7e65b1382babf75e95e28bf6d8ce
Janne <jannemieskolainen@gmail.com>
2021-08-31 10:49:05 +0300

    update & player

--- js/js.js
+++ js/js.js
@@ -4,5 +4,45 @@ var ctx = game.getContext("2d");
 document.getElementById("button").addEventListener("click", start);
 
 function start() {
-    console.log("Hello");
+    document.getElementById("button").disabled = true;
+    update();
+}
+
+var midx = game.width / 2;
+var midy = game.height / 2;
+
+const player = {
+    x: midx - 10,
+    y: midy - 10,
+    h: 20,
+    w: 20,
+    speed: 1,
+    color: "red",
+    png: null //Halutaanko kuvat myöhemmin?
+}
+
+function drawPlayer() {
+    ctx.fillStyle = player.color;
+    ctx.fillRect(player.x, player.y, player.w, player.h);
+}
+
+function clear() {
+    ctx.clearRect(0, 0, game.width, game.height)
+}
+
+function newPos() {
+
+
+    detectWalls();
+}
+
+function detectWalls() {
+
+}
+
+function update() {
+    clear();
+    drawPlayer();
+    newPos();
+    requestAnimationFrame(update);
 }
